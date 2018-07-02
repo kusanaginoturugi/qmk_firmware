ffe22c0c17285c643c3a4ed8f01ffdbdd5ee132f
Takuro onoue <kusanaginoturugi@gmail.com>
2018-07-03 01:02:09 +0900

    fix

--- keyboards/ergo42/keymaps/default/keymap.c
+++ keyboards/ergo42/keymaps/default/keymap.c
@@ -5,7 +5,7 @@ extern keymap_config_t keymap_config;
 #define BASE 0
 #define META 1
 #define SYMB 2
-#define GAME 3
+//#define GAME 3
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -13,58 +13,56 @@ extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
- /* BASE
-  * ,------------------------------------------------.   ,------------------------------------------------.
-  * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [   |   |  ]   |   Y  |   U  |   I  |   O  |   P  |  \?  |
-  * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-  * | Ctrl |   A  |   S  |   D  |   F  |   G  |      |   |      |   H  |   J  |   K  |   L  |  ;:  |  '"  |
-  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-  * | Sft  |   Z  |   X  |   C  |   V  |   B  |      |   |      |   N  |   M  |  ,<  |  .>  |  /?  |  `   |
-  * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-  * | META | SYMB | GUI  | Alt  |Muhen |Space | ESC  |   | META |Space |Enter | Pgup | Pgdn |Del   | BS   |
-  * `------------------------------------------------'   `------------------------------------------------'
-  */
-  [BASE] = KEYMAP( \
-    KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,   KC_Y,    /**/ KC_JYEN, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSLS, \
-    KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,     KC_G,   KC_LBRC, /**/ KC_RBRC, KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,   KC_N,    /**/ KC_B,    KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_GRV, \
-    META,    SYMB,   KC_LGUI, KC_LALT, KC_MUHEN, KC_SPC, KC_ESC,  /**/ META,    KC_SPC, KC_ENT, KC_PGUP, KC_PGDN, KC_DEL,  KC_BSPC \
+  /* BASE
+   * ,------------------------------------------------.   ,------------------------------------------------.
+   * | Tab  |   Q  |   W  |   E  |   R  |   T  |  [{  |   |  ]}  |   Y  |   U  |   I  |   O  |   P  |  BS  |
+   * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
+   * | Ctrl |   A  |   S  |   D  |   F  |   G  |  -_  |   |  =+  |   H  |   J  |   K  |   L  |  ;:  |  '"  |
+   * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
+   * | Sft  |   Z  |   X  |   C  |   V  |   B  | Alt  |   | Sft  |   N  |   M  |  ,<  |  .>  |  /?  |  \|  |
+   * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
+   * | META | SYMB | GUI  | Alt  |  `~  |Space | ESC  |   | META |Space |Enter | Pgup | Pgdn | Del  |Enter |
+   * `------------------------------------------------'   `------------------------------------------------'
+   */
+  [BASE] = LAYOUT( \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_LBRC, /**/ KC_RBRC,  KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_MINS, /**/ KC_EQL,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_RALT, /**/ KC_RSFT,  KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS, \
+    MO(META),MO(SYMB),KC_LGUI, KC_LALT, KC_GRV, KC_SPC, KC_ESC,  /**/ MO(META), KC_SPC, KC_ENT, KC_PGUP, KC_PGDN, KC_DELT, KC_ENT   \
   ),
-
   /* META
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | Reset|   1  |   2  |   3  |   4  |   5  |      |   |      |   6  |   7  |   8  |   9  |   0  |      |
+   * | Reset|   1  |   2  |   3  |   4  |   5  |   6  |   |   5  |   6  |   7  |   8  |   9  |   0  |      |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |      |   |      |  F6  |  F7  |  F8  |  F9  | F10  |      |
+   * | Ctrl |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   |  F5  |  F6  |  F7  |  F8  |  F9  | F10  |      |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | SYMB | F11  | F12  |      |      |      |      |   |      |      |      |      |      |      |      |
+   * | Sft  |  F11 |  F12 |      |      |      |      |   |      |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | META | SYMB | GUI  | Alt  |Muhen |Space | ESC  |   | META |Space |Enter | Pgup | Pgdn |Del   | BS   |
+   * | META | SYMB | GUI  | Alt  |  `~  |Space | ESC  |   | META |Space |Enter | Pgup | Pgdn | Del  |Enter |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-    RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LBRC,    KC_RBRC,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-    _______, KC_F1,   XXXXXXX, KC_MHEN, KC_HENK, XXXXXXX, XXXXXXX,    KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX, _______, \
-    _______, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   S(KC_LBRC), S(KC_RBRC), KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______ \
+    RESET,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______, \
+    _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
   ),
-
   /* SYMB
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | ESC  |   !  |   @  |   #  |   $  |   %  |  [   |   |  ]   |   ^  |   &  |   *  |   (  |   )  |      |
+   * | TAB  |   !  |   @  |   #  |   $  |   %  |  {   |   |  }   |   ^  |   &  |   *  |   (  |   )  |      |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   * | Del  |      |      |      |      |      |  (   |   |  )   |      |      |      |      |      |      |
+   * | CTRL |      |      |      |      |      |  [   |   |  ]   |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
-   * | SYMB |      |      |      |      |      |  {   |   |  }   |      |      |      |      |      |      |
+   * | SFT  |      |      |      |      |      |  (   |   |  )   |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * | META | SYMB | GUI  | Alt  |Muhen |Space | ESC  |   | META |Space |Enter | Pgup | Pgdn |Del   | BS   |
+   * | META | SYMB | GUI  | Alt  |  `~  |Space | ESC  |   | META |Space |Enter | Pgup | Pgdn | Del  |Enter |
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), KC_LBRC,    KC_RBRC,    S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_9),    S(KC_0),    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, \
-    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_LBRC), S(KC_RBRC), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______ \
+    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_LBRC), S(KC_RBRC), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC,    KC_RBRC,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, S(KC_9),    S(KC_0),    XXXXXXX, XXXXXXX, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______,    _______,    _______, _______, _______, _______, _______, _______  \
   )
 
 };
